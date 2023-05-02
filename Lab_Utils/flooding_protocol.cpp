55f845e8dd19a0bc5433beeafdc6e02467c56657
Padraig Lamont <padraiglamont@gmail.com>
2023-05-02 11:14:25 +0800

    Include the destination node ID in the packet header

--- Lab_Utils/flooding_protocol.cpp
+++ Lab_Utils/flooding_protocol.cpp
@@ -9,9 +9,6 @@
 #include "flooding_protocol.h"
 
 
-#define MAX_NUM_CHARS 32
-
-
 /**
  * Returns the index of the start of the next CSV field.
  */
@@ -56,116 +53,210 @@ bool str_to_int(const char *str, int *result) {
     return true;
 }
 
-PacketHeader * parsePacketHeader(char * packet) {
-    PacketHeader * result = (PacketHeader *) malloc(sizeof(PacketHeader));
+
+int parsePacketHeader(char * packet, char * expectedCommunicationID, PacketHeader * result) {
+    int index = 0;
+    bool success;
+    char numberBuffer[FP_MAX_PACKET_NUMBER_CHARS + 1];
+
+    // Read Communication ID.
+    char * communicationID = (char *) malloc(sizeof(char) * (FP_COMM_ID_LENGTH + 1));
+    if (communicationID == nullptr) {
+        return -1;
+    }
+
+    index = read_csv_field(packet, index, communicationID, FP_COMM_ID_LENGTH + 1);
+    result->communicationID = communicationID;
+    if (strcmp(communicationID, expectedCommunicationID) != 0) {
+        return -1;
+    }
+    
+    // Read Packet ID.
+    index = read_csv_field(packet, index, numberBuffer, FP_MAX_PACKET_NUMBER_CHARS);
+    success = str_to_int(numberBuffer, &result->packetID);
+    if (!success) {
+        return -1;
+    }
+
+    // Read Source Node ID.
+    index = read_csv_field(packet, index, numberBuffer, FP_MAX_PACKET_NUMBER_CHARS + 1);
+    success = str_to_int(numberBuffer, &result->sourceNodeID);
+    if (!success) {
+        return -1;
+    }
+
+    // Read Destination Node ID.
+    index = read_csv_field(packet, index, numberBuffer, FP_MAX_PACKET_NUMBER_CHARS + 1);
+    success = str_to_int(numberBuffer, &result->destNodeID);
+    if (!success) {
+        return -1;
+    }
+    
+    // Read Hop Count.
+    index = read_csv_field(packet, index, numberBuffer, FP_MAX_PACKET_NUMBER_CHARS);
+    success = str_to_int(numberBuffer, &result->hopCount);
+    if (!success) {
+        return -1;
+    }
+
+    return index;
+}
+
+
+int parsePacketNodeInfo(char * packet, int index, PacketNodeInfo * result) {
+    bool success;
+    printf("parsePacketNodeInfo\n");
+
+    // Read Node ID.
+    char numberBuffer[FP_MAX_PACKET_NUMBER_CHARS + 1];
+    index = read_csv_field(packet, index, numberBuffer, FP_MAX_PACKET_NUMBER_CHARS + 1);
+    success = str_to_int(numberBuffer, &result->nodeID);
+    if (!success) {
+        printf("a\n");
+        return -1;
+    }
+
+    // Read timestamp of packet.
+    index = read_csv_field(packet, index, numberBuffer, FP_MAX_PACKET_NUMBER_CHARS);
+    success = str_to_int(numberBuffer, &result->timestampMS);
+    if (!success) {
+        printf("b\n");
+        return -1;
+    }
+
+    // Read GPS Latitude.
+    index = read_csv_field(packet, index, numberBuffer, FP_MAX_PACKET_NUMBER_CHARS);
+    success = str_to_int(numberBuffer, &result->gpsLat);
+    if (!success) {
+        printf("c\n");
+        return -1;
+    }
+
+    // Read GPS Longitdue.
+    index = read_csv_field(packet, index, numberBuffer, FP_MAX_PACKET_NUMBER_CHARS);
+    printf("%s\n", numberBuffer);
+    success = str_to_int(numberBuffer, &result->gpsLon);
+    if (!success) {
+        printf("d\n");
+        return -1;
+    }
+
+    return index;
+}
+
+Packet * parsePacket(char * packet, char * expectedCommunicationID) {
+    Packet * result = (Packet *) malloc(sizeof(Packet));
     if (result == nullptr) {
         return nullptr;
     }
 
-    char * communicationID = (char *) malloc(sizeof(char) * (COMM_ID_LENGTH + 1));
-    if (communicationID == nullptr) {
+    int index = parsePacketHeader(packet, expectedCommunicationID, &result->header);
+    if (index < 0) {
         return nullptr;
     }
 
-    int index = 0;
-    bool success;
-
-    // Read communication ID.
-    index = read_csv_field(packet, index, communicationID, COMM_ID_LENGTH + 1);
-    result->communicationID = communicationID;
-
-    // Read timestamp of packet.
-    char * timestamp = (char *) malloc(sizeof(char) * (MAX_NUM_CHARS + 1));
-    if (timestamp == nullptr) {
-        return nullptr;
+    result->nodeInfoCount = result->header.hopCount;
+    if (result->nodeInfoCount == 0) {
+        result->nodeInfo = nullptr;
+        return result;
     }
-    index = read_csv_field(packet, index, timestamp, MAX_NUM_CHARS + 1);
-    result->time = timestamp;
 
-    // Read Node ID.
-    char numberBuffer[MAX_NUM_CHARS + 1];
-    index = read_csv_field(packet, index, numberBuffer, MAX_NUM_CHARS + 1);
-    success = str_to_int(numberBuffer, &result->nodeID);
-    if (!success) {
-        return nullptr;
-    }
-    
-    // Read Packet ID.
-    index = read_csv_field(packet, index, numberBuffer, MAX_NUM_CHARS);
-    success = str_to_int(numberBuffer, &result->packetID);
-    if (!success) {
-        return nullptr;
-    }
-    
-    // Read Hop Count.
-    index = read_csv_field(packet, index, numberBuffer, MAX_NUM_CHARS);
-    success = str_to_int(numberBuffer, &result->hopCount);
-    if (!success) {
+    PacketNodeInfo * nodeInfo = (PacketNodeInfo *) malloc(sizeof(PacketNodeInfo) * result->nodeInfoCount);
+    result->nodeInfo = nodeInfo;
+    if (nodeInfo == nullptr) {
         return nullptr;
     }
 
-    // Read GPS Latitude.
-    index = read_csv_field(packet, index, numberBuffer, MAX_NUM_CHARS);
-    success = str_to_int(numberBuffer, &result->gpsLat);
-    if (!success) {
-        return nullptr;
+    for (int hop = 0; hop < result->nodeInfoCount; ++hop) {
+        index = parsePacketNodeInfo(packet, index, &nodeInfo[hop]);
+        if (index < 0) {
+            return nullptr;
+        }
     }
-
-    // Read GPS Longitdue.
-    index = read_csv_field(packet, index, numberBuffer, MAX_NUM_CHARS);
-    success = str_to_int(numberBuffer, &result->gpsLon);
-    if (!success) {
-        return nullptr;
-    }
-
     return result;
 }
 
-void freePacketHeader(PacketHeader ** headerVar) {
-    PacketHeader *header = *headerVar;
-    free(header->communicationID);
-    free(header);
-    *headerVar = nullptr;
+
+void freePacket(Packet ** packetVar) {
+    Packet *packet = *packetVar;
+    if (!packet) {
+        return;
+    }
+
+    free(packet->header.communicationID);
+    free(packet->nodeInfo);
+    free(packet);
+    *packetVar = nullptr;
 }
 
-char * writePacketHeader(PacketHeader * header) {
-    int maxPacketLength = COMM_ID_LENGTH + 6 * (1 + MAX_NUM_CHARS);
-    char * packet = (char *) malloc(sizeof(char) * (maxPacketLength + 1));
-    if (packet == nullptr)
-        return nullptr;
+char * writePacket(Packet * packet) {
+    // Just allocate as much as we could possibly need,
+    // as it won't be that much memory anyway.
+    int maxPacketLength = FP_COMM_ID_LENGTH + 4 * (1 + FP_MAX_PACKET_NUMBER_CHARS);
+    for (int hop = 0; hop < packet->header.hopCount; ++hop) {
+        maxPacketLength += 4 * (1 + FP_MAX_PACKET_NUMBER_CHARS);
+    }
 
-    int written = snprintf(
-        packet,
-        maxPacketLength + 1,
-        "%s,%s,%d,%d,%d,%d,%d",
-        header->communicationID,
-        header->time,
-        header->nodeID,
-        header->packetID,
-        header->hopCount,
-        header->gpsLat,
-        header->gpsLon
-    );
-    if (written < 0 || written >= maxPacketLength) {
-        free(packet);
+    char * writtenPacket = (char *) malloc(sizeof(char) * (maxPacketLength + 1));
+    if (writtenPacket == nullptr) {
         return nullptr;
     }
-    packet[written] = '\0';
-    return packet;
+
+    // Write the packet header.
+    char * writeHead = writtenPacket;
+    int written = snprintf(
+        writeHead,
+        maxPacketLength + 1,
+        "%s,%d,%d,%d,%d",
+        packet->header.communicationID,
+        packet->header.packetID,
+        packet->header.sourceNodeID,
+        packet->header.destNodeID,
+        packet->header.hopCount
+    );
+    writeHead = &writeHead[written];
+    if (written < 0 || written >= maxPacketLength) {
+        free(writtenPacket);
+        return nullptr;
+    }
+
+    // Write each node info.
+    PacketNodeInfo * nodeInfos = packet->nodeInfo;
+    for (int hop = 0; hop < packet->header.hopCount; ++hop) {
+        PacketNodeInfo * nodeInfo = &nodeInfos[hop];
+        written = snprintf(
+            writeHead,
+            maxPacketLength + 1,
+            ",%d,%d,%d,%d",
+            nodeInfo->nodeID,
+            nodeInfo->timestampMS,
+            nodeInfo->gpsLat,
+            nodeInfo->gpsLon
+        );
+        writeHead = &writeHead[written];
+        if (written < 0 || written >= maxPacketLength) {
+            free(writtenPacket);
+            return nullptr;
+        }
+    }
+
+    writeHead[written] = '\0';
+    return writtenPacket;
 }
 
 int main() {
-    char * headerStr = (char *) "AAAA,00:00:00,123,256,777,100,101";
-    PacketHeader * header = parsePacketHeader(headerStr);
-    if (header) {
-        char * written = writePacketHeader(header);
+    char * expectedCommunicationID = (char *) "AAAA";
+    char * headerStr = (char *) "AAAA,122,123,256,1,3,4,5,6";
+    Packet * packet = parsePacket(headerStr, expectedCommunicationID);
+    if (packet) {
+        printf("%lld\n", packet->nodeInfo);
+        char * written = writePacket(packet);
 
-        printf("commID = %s\nnodeID = %d\npacketID = %d\nhopCount = %d\npacket = %s\n", header->communicationID, header->nodeID, header->packetID, header->hopCount, written);
+        printf("%s\n", written);
     } else {
-        printf("null\n");
+        printf("It broke\n");
     }
-    
-    freePacketHeader(&header);
 
+    freePacket(&packet);
     return 0;
 }
--- Lab_Utils/flooding_protocol.h
+++ Lab_Utils/flooding_protocol.h
@@ -2,25 +2,122 @@
 #define FLOODING_PROTOCOL_H
 
 
-#define COMM_ID_LENGTH 4
+/**
+ * The length of the communication ID field.
+ */
+#define FP_COMM_ID_LENGTH 4
+
+/**
+ * The maximum number of characters used to write a single number into a packet.
+ */
+#define FP_MAX_PACKET_NUMBER_CHARS 32
 
 
+/**
+ * One header per packet.
+ */
 typedef struct
 {
+    /**
+     * The communication ID is used to filter out irrelevant traffic.
+     */
     char * communicationID;
-    char * time;
-    int nodeID;
+
+    /**
+     * A unique ID for this packet.
+     */
     int packetID;
+
+    /**
+     * The ID of the node that last broadcast this packet.
+     */
+    int sourceNodeID;
+
+    /**
+     * The node ID that is the target destination of this packet.
+     */
+    int destNodeID;
+
+    /**
+     * The number of nodes that this packet has been forwarded from.
+     */
     int hopCount;
-    int gpsLat;
-    int gpsLon;
+
 } PacketHeader;
 
-PacketHeader * parsePacketHeader(char * packet);
 
-void freePacketHeader(PacketHeader ** header);
+/**
+ * One node info per each hop a packet has taken.
+ */
+typedef struct
+{
 
-char * writePacketHeader(PacketHeader * header);
+    /**
+     * The ID of this node that was hopped past.
+     */
+    int nodeID;
+
+    /**
+     * The time that this packet was sent, in milliseconds.
+     * This is only relevant to the node that the time was
+     * recorded on, as the timestamp is relative to when
+     * the node was turned on.
+     * 
+     * This will overflow and break at 24 days of uptime.
+     */
+    int timestampMS;
+
+    /**
+     * The latitude of this node, from the GPS.
+     */
+    int gpsLat;
+
+    /**
+     * The longitude of this node, from the GPS.
+     */
+    int gpsLon;
+
+} PacketNodeInfo;
+
+
+/**
+ * The whole packet, including a header and the
+ * info about each of node its hopped through.
+ */
+typedef struct
+{
+    /**
+     * The header of this packet.
+     */
+    PacketHeader header;
+
+    /**
+     * The number of past hops of this packet.
+     */
+    int nodeInfoCount;
+
+    /**
+     * An array of info about each node that was hopped through.
+     */
+    PacketNodeInfo * nodeInfo;
+} Packet;
+
+/**
+ * Read the packet header from the given packet text.
+ */
+Packet * parsePacket(char * packet, char * expectedCommunicationID);
+
+/**
+ * Frees the resources allocated with the given packet header.
+ */
+void freePacket(Packet ** header);
+
+/**
+ * Writes the given packet header to packet text to be transmitted.
+ */
+char * writePacket(Packet * header);
+
+
 
 int write_to_text(char * str);
 
